b936001fa94bed9aaced22d094d38d8c00abd48e
mAXBRAT34 <makbrat6@gmail.com>
2025-06-10 18:15:55 +0300

    sāka veikt testu

---
+++ src/TEST/Tests_DivdMasivs.java
@@ -0,0 +1,10 @@
+package TEST;
+
+import java.util.Scanner;
+
+public class Tests_DivdMasivs {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
